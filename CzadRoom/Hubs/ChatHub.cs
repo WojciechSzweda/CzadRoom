541c460d43a9b24d273e0a33fd94674181714c5e
wojtix144 <wojtix144@gmail.com>
2018-05-19 13:42:26 +0200

    rooms

--- CzadRoom/Hubs/ChatHub.cs
+++ CzadRoom/Hubs/ChatHub.cs
@@ -6,53 +6,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CzadRoom.Hubs
-{
+namespace CzadRoom.Hubs {
     [Authorize]
-    public class ChatHub : Hub
-    {
+    public class ChatHub : Hub {
         private readonly IRoomService _roomService;
         public ChatHub(IRoomService roomService) {
             _roomService = roomService;
         }
 
         public async Task SendMessage(string message) {
-            
-            await Clients.All.SendAsync("ReceiveMessage", Context.User.Identity.Name , message);
-        }
-
-        public async Task SendRoomMessage(string roomID, string message) {
-
             await Clients.All.SendAsync("ReceiveMessage", Context.User.Identity.Name, message);
         }
 
         public Task SendMessageToCaller(string message) {
-            return Clients.Caller.SendAsync("ReceiveServerMessage",  message);
+            return Clients.Caller.SendAsync("ReceiveServerMessage", message);
         }
 
-        public Task SendMessageToGroup(string message) {
-            List<string> groups = new List<string>() { "Group1" };
-            return Clients.Groups(groups).SendAsync("ReceiveMessage", message);
+        public Task SendRoomMessage(string roomId, string message) {
+            return Clients.Groups(roomId).SendAsync("ReceiveMessage", Context.User.Identity.Name, message, roomId);
         }
 
         public async Task JoinRoom(string roomId) {
-            //TODO: add user to room in db, so he can be removed when d/c
+            //TODO: add user to room in db
             await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
             await Clients.Group(roomId).SendAsync("ClientJoined", Context.User.Identity.Name);
         }
 
         public async Task LeaveRoom(string roomId) {
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomId);
-            await Clients.Group(roomId).SendAsync("ClientJoined", Context.User.Identity.Name);
+            await Clients.Group(roomId).SendAsync("ClientLeft", Context.User.Identity.Name);
         }
 
         public override async Task OnConnectedAsync() {
-            await Groups.AddToGroupAsync(Context.ConnectionId, "Group1");
+            await Clients.Caller.SendAsync("Connected");
             await base.OnConnectedAsync();
         }
 
         public override async Task OnDisconnectedAsync(Exception exception) {
-            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "Group1");
             await base.OnDisconnectedAsync(exception);
         }
     }
--- CzadRoom/wwwroot/js/chat.js
+++ CzadRoom/wwwroot/js/chat.js
@@ -2,7 +2,8 @@
     .withUrl("/chatHub")
     .build();
 
-connection.on("ReceiveMessage", (user, message) => {
+
+connection.on("ReceiveMessage", (user, message, roomId) => {
     const encodedMsg = user + " says " + message;
     const li = document.createElement("li");
     li.textContent = encodedMsg;
@@ -15,6 +16,27 @@ connection.on("ReceiveServerMessage", (message) => {
     li.textContent = encodedMsg;
     document.getElementById("messagesList").appendChild(li);
 });
+
+connection.on("ClientJoined", (client) => {
+    const encodedMsg = `${client} has joined`
+    const li = document.createElement("li")
+    li.textContent = encodedMsg
+    document.getElementById("messagesList").appendChild(li)
+})
+
+connection.on("ClientLeft", (client) => {
+    const encodedMsg = `${client} has left`
+    const li = document.createElement("li")
+    li.textContent = encodedMsg
+    document.getElementById("messagesList").appendChild(li)
+})
+
+connection.on("Connected", () => {
+    const roomID = document.getElementById("RoomID").value
+    connection.invoke("JoinRoom", roomID).catch(err => console.error(err.toString()))
+    console.log(`Joined room ${roomID}`)
+})
+
 
 document.getElementById("sendButton").addEventListener("click", event => {
     const roomID = document.getElementById("RoomID").value;
