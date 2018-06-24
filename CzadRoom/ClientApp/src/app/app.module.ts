216fce14998b007b99ca5898296aa0d2e10cf00a
wojtix144 <wojtix144@gmail.com>
2018-06-24 20:11:00 +0200

    navigation default

--- CzadRoom/ClientApp/src/app/app.module.ts
+++ CzadRoom/ClientApp/src/app/app.module.ts
@@ -11,7 +11,11 @@ import { ChatRoomComponent } from './chat-room/chat-room.component'
 import { PublicRoomComponent } from './public-room/public-room.component'
 import { DirectMessagesComponent } from './direct-messages/direct-messages.component'
 import { FriendsComponent } from './friends/friends.component'
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppNavComponent } from './app-nav/app-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material'
 
 @NgModule({
   declarations: [
@@ -21,7 +25,8 @@ import { HomeComponent } from './home/home.component'
     PublicRoomComponent,
     DirectMessagesComponent,
     FriendsComponent,
-    HomeComponent
+    HomeComponent,
+    AppNavComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +37,14 @@ import { HomeComponent } from './home/home.component'
       { path: 'public', component: PublicRoomComponent },
       { path: 'messages', component: DirectMessagesComponent },
       { path: 'friends', component: FriendsComponent },
-    ])
+    ]),
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
